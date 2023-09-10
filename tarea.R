57289a41ad032af0c73615955efc40a5c7e50983
DALILARUEDA <dalila.rueda.vet@gmail.com>
2023-09-09 21:54:02 -0300

    Me vi llegando tarde, tarde a todooo (commit de prueba la tarea esta mal)

--- tarea.R
+++ tarea.R
@@ -1,20 +1,80 @@
-library(shiny)
-library(tidyverse)
-library(tidyr)
-library(DT)
-library(lubridate)
-library(shinyWidgets)
-library(highcharter)
 
-#descargo el dataset 
+##ESTRUCTURA##
+niveles_prov <- unique(bd$prov)
 
-options(timeout=1000000) # incrementamos el timeout debido a que la descarga es lenta
+# Defino UI para mi aplicación
+ui <- fluidPage(
+  # Titulo
+  titlePanel("Mortalidad infantil en Argentina")
+  selectInput(
+    inputId = "niveles_prov",
+    label = "Seleccionar provincia:",
+    hc <- highchart() %>%
+      hc_chart(type = "line") %>%
+      hc_title(text = "Serie de tiempo de TMI por 1000 nacidos vivos, Argentina, periodo 1990-2021") %>%
+      hc_xAxis(title = list(text = "Año")) %>%
+      hc_yAxis(title = list(text = "TMI ")) %>%
+      hc_exporting(enabled = TRUE) # enable exporting option  
+    # Agrega una serie de datos para cada nivel de "prov"
+    for (nivel in niveles_prov) {
+      data_serie <- bd[bd$prov == nivel,]
+      hc <- hc %>%
+        hc_add_series(
+          data_serie,
+          "line",
+          hcaes(x = ano, y = TMI),
+          name = nivel,
+          marker = list(radius = 4)
+        )
+    }
+    
+    ## imprimo el grafico
+    #print(hc)
+    hc
+    
+  )
+  
+  )
 
-url = "http://datos.salud.gob.ar/dataset/2eff770c-1c2b-4a22-9281-c3b5e9412086/resource/c1253897-d507-41f7-a3e1-6ed756e7243b/download/tasa-mortalidad-infantil-deis-1990-2021.csv"
 
-download.file(url, destfile = "TMI.csv")
+#highchartOutput() ## para el UI
 
-data = read.csv("TMI.csv")
-unlink("TMI.csv")
 
-DT::datatable(data)
+# Defino server
+server <- function(input, output,session) {
+  renderHighchart(expr, env = parent.frame(), quoted = FALSE)
+  
+  renderHighchart() ## para el server  
+  
+}
+
+selectInput(
+  inputId = "niveles_prov",
+  label = "Seleccionar provincia:",
+  hc <- renderHighchart() %>%
+    hc_chart(type = "line") %>%
+    hc_title(text = "Serie de tiempo de TMI por 1000 nacidos vivos, Argentina, periodo 1990-2021") %>%
+    hc_xAxis(title = list(text = "Año")) %>%
+    hc_yAxis(title = list(text = "TMI ")) %>%
+    hc_exporting(enabled = TRUE) # enable exporting option  
+  # Agrega una serie de datos para cada nivel de "prov"
+  for (nivel in niveles_prov) {
+    data_serie <- bd[bd$prov == nivel,]
+    hc <- hc %>%
+      hc_add_series(
+        data_serie,
+        "line",
+        hcaes(x = ano, y = TMI),
+        name = nivel,
+        marker = list(radius = 4)
+      ),
+    
+  }
+  
+  ## imprimo el grafico
+  #print(hc)
+  hc
+
+shinyApp(ui, server)
+# Corro la application
+shinyApp(ui = ui, server = server)
